4b266f7f3a2c8e0285b0e6d7e74aba447980d07d
VolodymyrM7711 <volodymyrmendyk70@gmail.com>
2022-07-13 21:41:27 +0200

    GU-0001: added code structure and app icon

---
+++ Generators/Source/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Generators
+//
+//  Created by Volodymyr Mendyk on 13/07/2022.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+          Text("Hello world!")
+        } .background(.gray)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
